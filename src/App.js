38d2c8fa015c617b0a0cbc1ad5511a967f1033fd
brsckn1234 <brsckn1234@gmail.com>
2024-05-05 13:27:56 +0300

    first react project code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src='/logo192.png'></img>
+     <img src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
